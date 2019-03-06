56c9908b7d60c68873ef3ac421c03441550c5ce2
Nguyen Ha Chung <chung12dt@gmail.com>
2019-03-06 17:54:47 +0900

    update

--- CheckToken.php
+++ CheckToken.php
@@ -1,5 +1,9 @@
 <?php
-$token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTAsImhvdGVuIjoiY2h1bmciLCJlbWFpbCI6ImNodW5nMTJkdEBnbWFpbC5jb20ifQ.c9gkCYvn9YUACpjP4xdF0BUGAqmRj2ee7kv0lczJPyo";
+$token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTAsImhvdGVuIjoiY2h1bmciLCJlbWFpbCI6ImNodW5nMTJkdEBnbWFpbC5jb20ifQ.c9gkCYvn9YUACpjP4xdF0BUGAqmRj2ee7kv0lczJPygo";
 require("jwt.php");
-$json = JWT::decode($token, "MA_BI_MAT", true);
-echo json_encode($json);
+$secretKey = "MA_BI_MAT";
+try {
+	$json = JWT::decode($token, $secretKey, true);
+} catch (Exception $e) {
+	echo $e->getMessage();
+}
