a06b563a06a4f1777399a462bdd052f95ef8eefa
Nguyen <chung12dt@gmail.com>
2019-05-19 12:04:15 +0700

    TODO

--- CreateToken.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("jwt.php");
-$token = array();
-$token["id"] = 10;
-$token["hoten"] = "chung";
-$token["email"] = "chung12dt@gmail.com";
-
-$jsonWebToken = JWT::encode($token, "MA_BI_MAT");
-echo JsonHelper::getJson("token", $jsonWebToken);
